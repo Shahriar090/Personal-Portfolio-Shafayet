db0cd9dd05d0488eb9d9856e90094daeaaf0a9dc
Shahriar090 <shahriarh655@gmail.com>
2024-01-07 19:43:57 +0600

    Added date function to footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SocialIcons from "../Home/Banner/Social-Links/SocialIcons";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="w-full bg-[rgb(17,17,34)] border-t-2 border-black text-slate-300 p-8 flex items-center  flex-col md:flex-row lg:flex-row gap-4 justify-between">
       <div className="socials flex gap-4 text-slate-400 text-md md:text-lg lg:text-xl">
@@ -10,7 +11,7 @@ const Footer = () => {
       <div className="copyright">
         <p className="text-sm md:text-md lg:text-md">
           {" "}
-          © 2023 - All right reserved | Shafayet Hossain
+          © {currentYear} - All rights reserved | Shafayet Hossain
         </p>
       </div>
     </footer>
--- src/components/Skills/SkillCard.jsx
+++ src/components/Skills/SkillCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SkillCard = ({ skill }) => {
   const { id, image, title, description } = skill;
   return (
-    <div className="card w-full shadow-md  rounded-md  mx-auto pt-10 flex flex-col items-center">
+    <div className="card w-full rounded-md  mx-auto pt-10 flex flex-col items-center shadow-md shadow-slate-400">
       <picture className="w-3/5">
         <img
           src={image}
