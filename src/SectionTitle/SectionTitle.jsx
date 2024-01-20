608e8abbd0fe3fd32f556bea72b104dff7485b6e
Shahriar090 <shahriarh655@gmail.com>
2024-01-20 14:29:05 +0600

    Fixed a minor issue

--- src/SectionTitle/SectionTitle.jsx
+++ src/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SectionTitle = ({ heading }) => {
   return (
     <div className="py-12 md:py-16 lg:py-20">
-      <h1 className=" font-serif  uppercase font-bold text-3xl lg:text-4xl  text-center text-slate-100 underline underline-offset-8">
+      <h1 className=" font-serif  uppercase font-bold text-3xl  text-center text-slate-100 underline underline-offset-8">
         {heading}
       </h1>
     </div>
